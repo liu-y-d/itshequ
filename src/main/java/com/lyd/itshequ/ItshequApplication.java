f119932773d9dc7ee30bc467a5283a8f7a43d91b
SexJava <man021436@163.com>
2020-02-21 14:11:46 +0800

    项目初始化

---
+++ src/main/java/com/lyd/itshequ/ItshequApplication.java
@@ -0,0 +1,13 @@
+package com.lyd.itshequ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItshequApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItshequApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyd/itshequ/ItshequApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyd.itshequ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItshequApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
